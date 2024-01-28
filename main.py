1ceae86a2f61e7e0cbc4b9f5643ddf16d53acda5
Dmytro <ADimaso007@gmail.com>
2024-01-28 17:33:36 +0200

    start adding mobile support

--- main.py
+++ main.py
@@ -44,14 +44,48 @@ def renderPage():
                             preload=preload,
                             jquery=jquery), 200, {'Content-Type': 'text/html; charset=utf-8'}
 
+def renderPageMobile():
+    pgs = ""
+    style = ""
+    jquery = ""
+    preload = ""
+    mainjs = ""
+    with open("static/js/jquery.js", mode="r", encoding="utf-8") as f:
+        jquery = f.read()
+    with open("static/js/preload.js", mode="r", encoding="utf-8") as f:
+        preload = f.read()
+    with open("static/js/main.js", mode="r", encoding="utf-8") as f:
+        mainjs = f.read()
+    files = os.listdir("static/css/")
+    for i in files:
+        if i == "light.css":
+            continue
+        with open("static/css/"+i, mode="r", encoding="utf-8") as f:
+            style+=f.read()
+    for i in pages:
+        pgs += render_template(i + '.html')
+    return render_template('main.html', 
+                            styles=style, 
+                            pages=pgs,
+                            main=mainjs,
+                            preload=preload,
+                            jquery=jquery), 200, {'Content-Type': 'text/html; charset=utf-8'}
+
 preRenderedPage = ""
 
 @app.route('/', methods = ['GET'])
 def main():
-    global preRenderedPage
-    if preRenderedPage == "":
-        preRenderedPage = renderPage()
-    return preRenderedPage
+    client = request.user_agent
+    if "iPhone" in request.user_agent.string or "Android":
+        global preRenderedPageMobile
+        if preRenderedPageMobile == "":
+            preRenderedPageMobile = renderPageMobile()
+        return preRenderedPageMobile
+    else:
+        global preRenderedPage
+        if preRenderedPage == "":
+            preRenderedPage = renderPage()
+        return preRenderedPage
    
 
 if __name__=='__main__':
